46f71897f0af480fa6f5df3dd3dd0193e36705c1
Joseph Stewart <josephlikescode@gmail.com>
2023-08-16 11:39:29 -0700

    Add ssh keys to terraform configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+locals {
+  ssh_keys = zipmap(fileset("../ssh_keys", "*.pub"), [for fileName in fileset("../ssh_keys", "*.pub") : file("../ssh_keys/${fileName}")])
+}
+
 # Digital ocean resources
 resource "digitalocean_droplet" "do_hannah_web01" {
   backups    = false
@@ -17,6 +21,13 @@ resource "digitalocean_droplet" "do_hannah_web02" {
   image      = 48826207
   region     = "SFO2"
   monitoring = true
+}
+
+resource "digitalocean_ssh_key" "ssh_keys" {
+  for_each = local.ssh_keys
+
+  name = each.key
+  public_key = each.value
 }
 
 resource "local_file" "ansible_inventory" {
