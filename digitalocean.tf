fe8223a940a568203ae18c45b2480ee4cff04d2b
Joseph Stewart <josephlikescode@gmail.com>
2023-08-16 10:32:19 -0700

    Add comment explaining funky image number.

--- digitalocean.tf
+++ digitalocean.tf
@@ -11,6 +11,8 @@ resource "digitalocean_droplet" "do_hannah_web02" {
   backups    = false
   name       = "do-hannah-web02"
   size       = "s-1vcpu-1gb"
+
+  # Imported resource, that's why this image is a funky number
   image      = 48826207
   region     = "SFO2"
   monitoring = true
