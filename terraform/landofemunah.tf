7d501216c477408772094a66ca7f4de1ede6c792
Joseph Stewart <josephlikescode@gmail.com>
2023-08-26 13:57:06 -0700

    Put public LOE resources behind cloudfront cdn.

--- terraform/landofemunah.tf
+++ terraform/landofemunah.tf
@@ -1,5 +1,9 @@
 # Resources specifically for land of emunah (DNS, captcha, etc)
 
+locals {
+  s3_cdn_id = "loeS3Origin"
+}
+
 resource "google_service_account" "loe_service_account" {
   account_id  = "land-of-emunah"
   description = "The account for managing land of emunah resources"
@@ -33,16 +37,85 @@ resource "aws_s3_bucket_ownership_controls" "loe_public_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "loe_public_bucket" {
   bucket                  = aws_s3_bucket.loe_public_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "loe_public_bucket" {
   depends_on = [aws_s3_bucket_ownership_controls.loe_public_bucket]
   bucket     = aws_s3_bucket.loe_public_bucket.id
-  acl        = "public-read"
+  acl        = "private"
+}
+
+resource "aws_cloudfront_origin_access_identity" "loe_cdn" {
+}
+
+resource "aws_cloudfront_distribution" "loe_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.loe_public_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_cdn_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.loe_cdn.cloudfront_access_identity_path
+    }
+  }
+
+  enabled = true
+
+  price_class = "PriceClass_100"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.s3_cdn_id
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+}
+
+data "aws_iam_policy_document" "loe_cdn_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.loe_cdn.iam_arn]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.loe_public_bucket.arn,
+      "${aws_s3_bucket.loe_public_bucket.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "loe_cdn_access" {
+  bucket = aws_s3_bucket.loe_public_bucket.id
+  policy = data.aws_iam_policy_document.loe_cdn_policy.json
 }
 
 resource "aws_s3_bucket" "loe_submissions_bucket" {
